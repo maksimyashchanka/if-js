da3dffbaa248ad661cd3a4271f3a0275890a534c
Макс <maksimyashchanka@gmail.com>
2021-12-04 21:09:46 +0300

    test

--- main.js
+++ main.js
@@ -1,1 +1,69 @@
+/*const user = ("John Doe");
+console.log(user) //John Doe
+
+
+const student = ("Maksim");
+console.log( student) // Maksim
+
+
+user2 = (student);
+console.log(user2) // Maksim
+
+
+let test = (1);
+let test2 = (test++);
+console.log(test) //2
+
+
+let test3 = (1);
+let test4 = test3--;
+console.log(test3) //0
+
+
+const test6 = Boolean ("");
+console.log( typeof test6)  // Boolean
+
+
+let result = 1;
+let arr = [2, 3, 5, 8 ]
+    for (let i = 0; i < arr.length; i++) {
+        result *= arr[i]
+    }
+    console.log(result) //240
+*/
+
+
+
+let lol = [2, 5, 8, 15, 0, 6, 20, 3]
+    for( let i = 0; i < lol.length; i++) {
+        if(lol[i] > 5 && lol[i] < 10) {
+            console.log(lol[i]);
+
+        }
+    }
+
+
+
+    /*
+let num = [2, 5, 8, 15, 0, 6, 20, 3]
+    for( let i = 0; i < num.length; i++ )
+    {
+        if ((num[i] % 2 ) === 0)
+        {
+            console.log(num[i]); //2 8 0 6 20
+        }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
